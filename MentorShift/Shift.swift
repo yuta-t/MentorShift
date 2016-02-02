96a3ba2989648985eb0c7e1e3d7b3c248366c3ab
yuta-t <yutatuy.t@gmail.com>
2016-02-03 02:32:00 +0900

    [add] Shift.fetch()とiniti(json)を記述

--- MentorShift/Shift.swift
+++ MentorShift/Shift.swift
@@ -7,8 +7,11 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 struct Shift {
+    static let url = "http://localhost:3000/api/shift_app/mentor_shifts"
+    
     let datetime: NSDate
     let role: Role
     
@@ -21,6 +24,31 @@ struct Shift {
     var time: String {
         let comp = datetime.convertToComponents(.Hour)
         return "\(comp.hour)時 - \(comp.hour + 2)時"
+    }
+    
+    init(json: JSON) {
+        let date = json["date"].stringValue
+        let time = json["time"].stringValue
+        let role = json["role"].stringValue
+        
+        datetime = NSDate.convertFromDateString("\(date) \(time)")
+        self.role = Role(rawValue: role)!
+    }
+    
+    static func fetch() -> [Shift] {
+        var shifts: [Shift] = []
+        
+        let params = ["user_id": 55]
+        WebAPI.responseJSON(Shift.url, parameters: params) { result in
+            guard let result = result else { return }
+            
+            for (_, json) in JSON(result) {
+                let shift = Shift(json: json)
+                shifts.append(shift)
+            }
+        }
+        
+        return shifts
     }
 }
 
